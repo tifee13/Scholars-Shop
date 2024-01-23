7734e90e1becd79d32d9f3306046e920702dab9e
Boluwatife <boluwatife.o-oluyomi@kibo.school>
2024-01-23 15:49:07 +0100

    Another

--- scholars_shop/app.py
+++ scholars_shop/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+   
 @app.route('/')
-def index():
+def homepage():
     return render_template('index.html')
+
+@app.route('/register')
+def register_page():
+    return render_template ('register.html') 
+
+@app.route('/signin')
+def signin_page():
+    return render_template ('signin.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
