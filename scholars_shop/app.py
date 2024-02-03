f2298d27547fad9b850ec480ca7c478379ebe86b
oluwapoloreoyeniji <oluwapolorealexander@gmail.com>
2024-02-03 21:01:11 +0100

    defines market route

--- scholars_shop/app.py
+++ scholars_shop/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
    
 @app.route('/')
+@app.route('/home')
 def homepage():
     return render_template('index.html')
 
@@ -13,6 +14,10 @@ def register_page():
 @app.route('/login')
 def login_page():
     return render_template ('login.html') 
+
+@app.route('/market')
+def market():
+    return render_template('market.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
