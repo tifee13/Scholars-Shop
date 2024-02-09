399ad6564d128e3446709783f21a670d8c4932db
Boluwatife <boluwatife.o-oluyomi@kibo.school>
2024-02-09 07:12:03 +0100

    Database implementation start-up

--- scholars_shop/app.py
+++ scholars_shop/app.py
@@ -1,18 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+import sqlite3
+
 
 app = Flask(__name__)
-   
+
+def create_database():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT NOT NULL,
+                    password TEXT NOT NULL
+                 )''')
+                 
+    conn.commit()
+    conn.close()
+
 @app.route('/')
 @app.route('/home')
 def homepage():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
-    return render_template ('register.html') 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('scholars.db')
+        c = conn.cursor()
 
-@app.route('/login')
+        c.execute("SELECT * FROM users WHERE username=?", (username,))
+        existing_user = c.fetchone()
+
+        if existing_user:
+            conn.close()
+            return "Username already exists. Please choose a different one."
+        
+        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('login_page'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        conn = sqlite3.connect('scholars.db')
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        user = c.fetchone()
+
+        conn.close()
+
+        if user:
+            return "Login successful"
+        else:
+            return "Invalid username or password"
+        
     return render_template ('login.html') 
 
 @app.route('/food')
@@ -25,4 +76,5 @@ def fashionpage():
 
 
 if __name__ == '__main__':
+    create_database()
     app.run(debug=True)
