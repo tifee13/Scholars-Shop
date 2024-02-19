8b555df10f595e71e0d79b20302e782f4c805c17
Boluwatife <boluwatife.o-oluyomi@kibo.school>
2024-02-19 18:14:08 +0100

    All links are valid

--- scholars_shop/app.py
+++ scholars_shop/app.py
@@ -13,40 +13,6 @@ def create_database():
                     username TEXT NOT NULL,
                     password TEXT NOT NULL
                  )''')
-                 
-    conn.commit()
-    conn.close()
-
-def create_products_database():
-    conn = sqlite3.connect('products.db')
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS products (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL,
-                    description TEXT NOT NULL,
-                    image BLOB NOT NULL,
-                    cat_id INTEGER NOT NULL,
-                 )''')
-
-    # Seed products data
-    sample_products = [
-        ('Product 1', 'Description 1', 'image1.jpg', 1),
-        ('Product 2', 'Description 2', 'image2.jpg', 2),
-        ('Product 3', 'Description 3', 'image3.jpg', 3),
-        ('Product 4', 'Description 4', 'image4.jpg', 4),
-        ('Product 5', 'Description 5', 'image5.jpg', 5),
-        ('Product 6', 'Description 6', 'image6.jpg', 1)
-    ]
-
-    c.executemany("INSERT INTO products (name, description, image, cat_id) VALUES (?, ?, ?, ?)", sample_products)
-                 
-    conn.commit()
-    conn.close()
-
-def create_categories_database():
-    conn = sqlite3.connect('categories.db')
-    c = conn.cursor()
 
     c.execute('''CREATE TABLE IF NOT EXISTS categories (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -64,10 +30,33 @@ def create_categories_database():
 
     c.executemany("INSERT INTO categories (name) VALUES (?)", categories)
 
+    c.execute('''CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT NOT NULL,
+                image BLOB NOT NULL,
+                category TEXT NOT NULL,
+                FOREIGN KEY (category) REFERENCES categories(name2)
+                )''')
+    
+    # Seed products data
+    sample_products = [
+        ('Product 1', 'Description 1', 'image1.jpg', 1),
+        ('Product 2', 'Description 2', 'image2.jpg', 2),
+        ('Product 3', 'Description 3', 'image3.jpg', 3),
+        ('Product 4', 'Description 4', 'image4.jpg', 4),
+        ('Product 5', 'Description 5', 'image5.jpg', 5),
+        ('Product 6', 'Description 6', 'image6.jpg', 1)
+    ]
+
+    c.executemany("INSERT INTO products (name, description, image, category) VALUES (?, ?, ?, ?)", sample_products)
+
     conn.commit()
     conn.close()
 
+
 @app.route('/')
+
 @app.route('/home')
 def home():
     return render_template('index.html')
@@ -116,6 +105,56 @@ def login():
         
     return render_template ('login.html') 
 
+@app.route('/food')
+def show_food():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor() 
+    c.execute('SELECT * FROM products WHERE category="Food"')
+    food_items = c.fetchall()
+    conn.close()
+    
+    return render_template('food.html', food_items=food_items)
+
+@app.route('/fashion')
+def show_fashion():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor() 
+    c.execute('SELECT * FROM products WHERE category="Fashion"')
+    fashion_items = c.fetchall()
+    conn.close()
+    
+    return render_template('fashion.html', fashion_items=fashion_items)
+
+@app.route('/computing')
+def show_computing():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor() 
+    c.execute('SELECT * FROM products WHERE category="Computing"')
+    computing_items = c.fetchall()
+    conn.close()
+    
+    return render_template('computing.html', computing_items=computing_items)
+
+@app.route('/phones')
+def show_phones():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor() 
+    c.execute('SELECT * FROM products WHERE category="Phones"')
+    phones_items = c.fetchall()
+    conn.close()
+    
+    return render_template('phones.html', phones_items=phones_items)
+
+@app.route('/electronics')
+def show_electronics():
+    conn = sqlite3.connect('scholars.db')
+    c = conn.cursor() 
+    c.execute('SELECT * FROM products WHERE category="Electronics"')
+    electronics_items = c.fetchall()
+    conn.close()
+    
+    return render_template('phones.html', electronics_items=electronics_items)
+
 @app.route('/products/<cat_id>')
 def show_products():
     cat_id = request.args.get('cat_id')
@@ -124,7 +163,7 @@ def show_products():
         cat_id = int(cat_id)
 
         # Fetch products based on the specified category
-        conn = sqlite3.connect('products.db')
+        conn = sqlite3.connect('scholars.db')
         c = conn.cursor()
 
         c.execute("SELECT * FROM products WHERE cat_id=?", (cat_id,))
@@ -134,7 +173,7 @@ def show_products():
 
         return render_template('products.html', products=products_data, category_id=cat_id)
     else:
-        conn = sqlite3.connect('products.db')
+        conn = sqlite3.connect('scholars.db')
         c = conn.cursor()
 
         c.execute("SELECT * FROM products")
@@ -146,6 +185,4 @@ def show_products():
 
 if __name__ == '__main__':
     create_database()
-    create_categories_database()
-    create_products_database()
     app.run(debug=True)
